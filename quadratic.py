5a747a4550792e00b272e5a6dc53eee5f11a898a
isso0424 <isso04240129@gmail.com>
2019-10-27 22:18:35 +0900

    create quadratics base and [WIP]equation

--- quadratic.py
+++ quadratic.py
@@ -1,1 +1,61 @@
 # TODO:はよ書け!!!
+from basic import int_input
+import math
+
+
+class quadratic:
+    """
+    Parameter
+    ---------
+    self.a : int
+        it is a of ax^2 + bx + c
+    self.b : int
+        it is b of ax^2 + bx + c
+    self.c : int
+        it is c of ax^2 + bx + c
+    """
+    def __init__(self):
+        formula = "ax^2 + bx + c"
+        self.a = int_input("please input a of {}".format(formula))
+        self.b = int_input("please input b of {}".format(formula))
+        self.c = int_input("please input c of {}".format(formula))
+
+    def equation(self):
+        """
+        Return
+        ------
+        solution : list
+            solution of quadratic equation
+            [0] : -b
+            [1] : b^2 ^4ac
+            [2] : 2a
+            => ([0] +- [1]) / [2] is true solution
+        """
+        solution = [-self.b,
+                    self.b**2 - 4 * self.a * self.c,
+                    self.a * 2]
+        return solution
+
+    def equation_value(self):
+        """
+        Return
+        ------
+        solution : list(float)
+            solution value of quadratic equation
+        """
+        D = self.b ** 2 - 4 * self.a * self.c
+        solution = []
+        if D > 0:
+            solution.append((-self.b + pow(self.b**2 - 4 * self.a * self.c, 0.5)) / 2 * self.a)
+            solution.append((-self.b - pow(self.b**2 - 4 * self.a * self.c, 0.5)) / 2 * self.a)
+        elif D == 0:
+            solution.append(-self.b / (2 * self.a))
+        else:
+            solution.append((-self.b + pow(self.b**2 - 4 * self.a * self.c, 0.5)))
+            solution.append((-self.b + pow(self.b**2 - 4 * self.a * self.c, 0.5)))
+        return solution
+
+
+if __name__ == "__main__":
+    test = quadratic()
+    test.equation()
